17c23c0c788db4cec3cb231ef3e491bea6766b27
EduardoAjcajon <126490019+EduardoAjcajon@users.noreply.github.com>
2023-07-21 10:41:58 -0600

    Add files via upload

---
+++ contenido/contenido.js
@@ -0,0 +1,4 @@
+let contenido_DOM = document.querySelector(".contenido");
+
+let contenido = '<div class="caja-img"><h3>Bus y transportes pesados</h3><span>🚌</span></div><form class = "imputs"><input type="text" class="placa"><input type="time" class="inputEntrada"><button class = "boton">ingresar</button></form>';
+contenido_DOM.innerHTML = contenido;
